bd9fb8515a15a49db853bbe15d25b13fc9bebd18
Brian Fan <kuomu.fan@yahoo.com>
2017-10-12 11:56:40 -0700

    edit babelrc file and add js module example

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import './assets/scss/app.scss';
+import { sum } from './assets/js/sum'; //import sum js module
 import 'jquery';
 import 'bootstrap'; // importing bootstrap.js
 
 import 'bootstrap/scss/bootstrap.scss'; // bootstrap.scss
+
+const total = sum(10,5);
+console.log(total);
--- webpack.config.js
+++ webpack.config.js
@@ -23,62 +23,46 @@ const config = {
 
   module: {
     rules: [
-       //babel-loader
-       {
-         test: /\.js$/,
-         include: /src/,
-         exclude: /node_modules/,
-         use: {
-           loader: "babel-loader",
-           options: {
-             presets: ['env']
-           }
-         }
-       },
-       //html-loader
-       { test: /\.html$/, use: ['html-loader'] },
-       //sass-loader
-       {
-         test: /\.scss$/,
-        //  include: [path.resolve(__dirname, 'src', 'assets', 'scss')],
-         use: extractPlugin.extract({
-           use: [
-             {
-               loader: 'css-loader',
-               options: {
-                 sourceMap: true
-               }
-             },
-             {
-               loader: 'sass-loader',
-               options: {
-                 sourceMap: true
-               }
-             }
-           ],
-           fallback: 'style-loader'
-         })
-       },
-       //file-loader(for images)
-       {
-         test: /\.(jpg|png|gif|svg)$/,
-         use: [
-           {
-             loader: 'file-loader',
-             options: {
-               name: '[name].[ext]',
-               outputPath: './assets/media/'
-             }
-           }
-         ]
-       },
-       //file-loader(for fonts)
-       {
-         test: /\.(woff|woff2|eot|ttf|otf)$/,
-         use: ['file-loader']
-       }
+
+      // babel-loader with 'env' preset
+      {
+        test: /\.js$/,
+        include: /src/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      // html-loader
+      { test: /\.html$/, use: ['html-loader'] },
+      // sass-loader with sourceMap activated
+      {
+        test: /\.scss$/,
+        // include: [path.resolve(__dirname, 'src', 'assets', 'scss')],
+        use: extractPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ],
+          fallback: 'style-loader'
+        })
+      },
+      // file-loader(for images)
+      { test: /\.(jpg|png|gif|svg)$/, use: [ { loader: 'file-loader', options: { name: '[name].[ext]', outputPath: './assets/media/' } } ] },
+      // file-loader(for fonts)
+      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] }
+
     ]
   },
+
 
   plugins: [
     new CleanWebpackPlugin(['dist']),
@@ -98,6 +82,7 @@ const config = {
   ],
 
   devServer: {
+    // static files served from here
     contentBase: path.resolve(__dirname, "./dist/assets/media"),
     compress: true,
     port: 8080,
